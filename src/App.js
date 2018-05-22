ee017714fe1990371de4abc4d9e85b6be4a3aeb3
natan-mlk <adam.milko@gmail.com>
2018-05-22 23:10:03 +0200

    added: example navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Navigation from './navigation'
 import Gallery1 from './galleries/gallery1'
-import Gallery2 from './galleries/gallery2'
-import Gallery3 from './galleries/gallery3'
+// import Gallery2 from './galleries/gallery2'
+// import Gallery3 from './galleries/gallery3'
 
 import './styles/App.css';
 
-class App extends Component {
-    render() {
-        return (
-            <Router basename="">
-                <div className="App">
-                    <Navigation/>
+const App = () => {
 
-                    <div className="content">
-                        <Route exact path="/" component={Gallery1}/>
-                        <Route path="/bio" component={Gallery2}/>
-                        <Route path="/sleep" component={Gallery3}/>
-                    </div>
+    return (
+        <Router>
+            <div className="App">
+                <Navigation/>
+
+                <div className="content">
+                    <Route exact path="/" component={Gallery1}/>
+                    {/*<Route path="/gallery2" component={Gallery2}/>*/}
+                    {/*<Route path="/gallery3" component={Gallery3}/>*/}
                 </div>
-            </Router>
-        );
-    }
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/galleries/gallery1.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Gallery1 = () => {
+    return (
+        <div> kkkk</div>
+    )
+}
+
+export default Gallery1
