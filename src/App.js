28d505ef747b972c0694176841724aadfebc1f26
natan-mlk <adam.milko@gmail.com>
2018-05-22 22:38:18 +0200

    basic structure of files and imports

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Navigation from './navigation'
+import Gallery1 from './galleries/gallery1'
+import Gallery2 from './galleries/gallery2'
+import Gallery3 from './galleries/gallery3'
+
+import './styles/App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router basename="">
+                <div className="App">
+                    <Navigation/>
+
+                    <div className="content">
+                        <Route exact path="/" component={Gallery1}/>
+                        <Route path="/bio" component={Gallery2}/>
+                        <Route path="/sleep" component={Gallery3}/>
+                    </div>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
