c7348343f645e69c2becedd698c3ea76cbcf8737
natan-mlk <adam.milko@gmail.com>
2018-05-26 23:10:53 +0200

    - work in progress -

--- src/galleries/gallery3.js
+++ src/galleries/gallery3.js
@@ -1,10 +1,66 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Grid, Row} from 'react-bootstrap';
+import IMAGES from './all-images'
+
+function imagesLoaded(parentNode) {
+    const imgElements = [...parentNode.querySelectorAll("img")];
+    for (const img of imgElements) {
+        if (!img.complete) {
+            return false;
+        }
+    }
+    return true;
+}
+
+class Gallery3 extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+        };
+    }
+
+    renderSpinner() {
+        if (!this.state.loading) {
+            // Render nothing if not loading
+            return null;
+        }
+        return (
+            <div>SPINNER</div>
+        );
+    }
+
+    handleStateChange = () => {
+        this.setState({
+            loading: !imagesLoaded(this.galleryElement),
+        });
+    }
 
 
-const Gallery3 = () => {
-    return (
-        <div> fwefewfwe</div>
-    )
+
+    render() {
+        return (
+            <Grid>
+                <Row ref={element => { this.galleryElement = element; }}>
+                    {this.renderSpinner()}
+
+                    {IMAGES.map((item, index) => {
+                        return (
+                            <div className="image-box" >
+                                <img src={item.src}
+                                     key={index}
+                                     onLoad={this.handleStateChange}
+                                     onError={this.handleStateChange}
+                                     className="gallery-image"/>
+                            </div>
+                        )
+                    })}
+                </Row>
+            </Grid>
+        )
+    }
+
 }
 
 export default Gallery3
