18618a48ddb0880b03a1f2a6cc8094b2a3b0c7e8
natan-mlk <adam.milko@gmail.com>
2018-05-27 20:27:59 +0200

    more images in all-images.js + bacis media queries added

--- src/galleries/all-images.js
+++ src/galleries/all-images.js
@@ -8,20 +8,26 @@ const images = [
     {src: 'https://placeimg.com/500/300/nature?7', cat: 'nature', caption:'A very long caption this time to show how' +
     ' will it behave if the line is broken'},
     {src: 'https://placeimg.com/500/300/nature?8', cat: 'nature', caption:'Something here'},
-    {src: 'https://placeimg.com/500/300/nature?8', cat: 'nature', caption:'Nice one'},
+    {src: 'https://placeimg.com/500/300/nature?9', cat: 'nature', caption:'Nice one'},
+
     {src: 'https://placeimg.com/640/480/people?1', cat: 'people', caption:'Nice one'},
-    {src: 'https://placeimg.com/640/480/people?2', cat: 'people', caption:'Nice one'},
-    {src: 'https://placeimg.com/640/640/people?3', cat: 'people', caption:'Nice one'},
-    {src: 'https://placeimg.com/640/480/people?4', cat: 'people', caption:'Nice one'},
-    {src: 'https://placeimg.com/640/480/people?5', cat: 'people', caption:'Nice one'},
-    {src: 'https://placeimg.com/640/600/people?6', cat: 'people', caption:'Nice one'},
-    {src: 'https://placeimg.com/640/480/people?7', cat: 'people', caption:'Nice one'},
+    {src: 'https://placeimg.com/640/480/people?2', cat: 'people', caption:'Something here'},
+    {src: 'https://placeimg.com/640/640/people?3', cat: 'people', caption:'Beautiful'},
+    {src: 'https://placeimg.com/640/480/people?4', cat: 'people', caption:'Batman'},
+    {src: 'https://placeimg.com/640/480/people?5', cat: 'people', caption:'Another photo'},
+    {src: 'https://placeimg.com/640/600/people?6', cat: 'people', caption:'Like anything'},
+    {src: 'https://placeimg.com/640/480/people?7', cat: 'people', caption:'People'},
+    {src: 'https://placeimg.com/640/480/people?8', cat: 'people', caption:'More people'},
+
     {src: 'https://placeimg.com/500/300/arch?1', cat: 'arch', caption:'A very long caption this time to show how' +
     ' will it behave if line is broken'},
     {src: 'https://placeimg.com/500/300/arch?2', cat: 'arch', caption:'Nice one'},
-    {src: 'https://placeimg.com/500/300/arch?3', cat: 'arch', caption:'Nice one'},
-    {src: 'https://placeimg.com/500/300/arch?4', cat: 'arch', caption:'Nice one'},
-    {src: 'https://placeimg.com/500/300/arch?5', cat: 'arch', caption:'Nice one'},
+    {src: 'https://placeimg.com/500/300/arch?3', cat: 'arch', caption:'Batman'},
+    {src: 'https://placeimg.com/500/300/arch?4', cat: 'arch', caption:'Like anything'},
+    {src: 'https://placeimg.com/500/300/arch?5', cat: 'arch', caption:'Beautiful'},
+    {src: 'https://placeimg.com/500/300/arch?6', cat: 'arch', caption:'Error 404'},
+    {src: 'https://placeimg.com/500/300/arch?7', cat: 'arch', caption:'Error 500'},
+    {src: 'https://placeimg.com/500/300/arch?8', cat: 'arch', caption:'Another Batman'},
 ];
 
 export default images
